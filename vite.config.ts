72da1b005ed732db9e85afe4e652792c90b0a642
Rithrita-Kong <kongrithrita@gmail.com>
2024-06-09 00:53:30 +0700

    fix: Fix base url for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-todo-app/",
+  base: "/React-Todo-App/",
   plugins: [react()],
   test: {
     globals: true,
